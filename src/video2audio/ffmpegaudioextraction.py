a10a3a3b38df1a0480c3f82147b5deebce8ce0ee
Florian Buetow <2320560+fbcom@users.noreply.github.com>
2023-03-01 23:53:34 +0000

    Adding PCM16 (wav) support to allow for whisper to skip preprocessing time during transcription

--- src/video2audio/ffmpegaudioextraction.py
+++ src/video2audio/ffmpegaudioextraction.py
@@ -7,21 +7,31 @@ class FFmpegAudioExtraction:
     The convert() method takes a source file and a destination file as input, and uses the ffmpeg tool to
     convert the source file to mp3 format, storing the result in the destination file.
 
-    For now, the conversion settings are hardcoded in the constructor.
+    The supported target audio formats and their ffmpeg conversion settings are hardcoded in the constructor.
+
+    Note: The specified wav format is the only format that can be read by whisper without any further pre-processing.
+    If whisper is given another audio format such as an MP3, it will use ffmpeg to convert the MP3 into this PCM wav format before transcribing.
     """
 
     def __init__(self):
-        self._conversion_settings = "-vn -acodec libmp3lame -ac 2 -ab 160k -ar 48000"
+        # List of supported target formats and their ffmpeg conversion settings
+        self._conversion_settings = {
+            '.mp3': '-vn -acodec libmp3lame -ac 2 -ab 160k -ar 48000',
+            '.wav': '-vn -ar 16000 -ac 1 -c:a pcm_s16le'
+        }
 
     def convert_to_audio(self, src_file: str, dst_file: str, overwrite: bool = False) -> bool:
+        print("Converting file '{}' to '{}'.".format(src_file, dst_file))
         # get the extension of the destination file
-        dst_extension = os.path.splitext(dst_file)[1]
-        if dst_extension == '.mp3':
-            return self.convert_to_mp3(src_file, dst_file, overwrite)
+        dst_extension = os.path.splitext(dst_file)[1].lower()
+        print("dst_extension: {}".format(dst_extension))
+        if dst_extension in self._conversion_settings:
+            return self._convert_with_ffmpeg(src_file, dst_file, self._conversion_settings[dst_extension], overwrite)
         else:
             raise Exception(f"Unsupported audio format '{dst_extension.upper()}'.")
 
-    def convert_to_mp3(self, src_file: str, dst_file: str, overwrite: bool = False) -> bool:
+
+    def _convert_with_ffmpeg(self, src_file: str, dst_file: str, conversion_settings: str, overwrite: bool = False) -> bool:
         if os.path.exists(dst_file):
             if overwrite:
                 os.remove(dst_file)
@@ -31,7 +41,7 @@ class FFmpegAudioExtraction:
 
         src_file_escaped = self._escape_path(src_file)
         dst_file_escaped = self._escape_path(dst_file)
-        cmd = f"ffmpeg -hide_banner -loglevel error -i {src_file_escaped} {self._conversion_settings} {dst_file_escaped}"
+        cmd = f"ffmpeg -hide_banner -loglevel error -i {src_file_escaped} {conversion_settings} {dst_file_escaped}"
         response = os.system(cmd)
         if response != 0:
             logging.error(f"Error converting file '{src_file}' to '{dst_file}'.")
